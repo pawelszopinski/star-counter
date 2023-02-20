ce8b435435fd96807db7d343515b472b9a077494
Paweł Szopiński <szopinskipawel@gmail.com>
2023-02-20 01:05:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ let saveEL = document.getElementById("save-el");
 let CountButtonEl = document.getElementById("btn-increment");
 let SaveButtonEl = document.getElementById("btn-save");
 
-CountButtonEl.addEventListener("click", function increment() {
-    count += 1;
-    countEl.textContent = count;
-});
-SaveButtonEl.addEventListener("click", function saveString() {
-    let countStr = count + " - ";
-    saveEL.textContent += countStr;
-    count = 0
-    countEl.textContent = 0;
-});
+CountButtonEl.addEventListener("click", () => {
+        count += 1;
+        countEl.textContent = count;
+    });
+SaveButtonEl.addEventListener("click", () => {
+        let countStr = `${count} - `;
+        saveEL.textContent += countStr;
+        count = 0;
+        countEl.textContent = 0;
+    });
